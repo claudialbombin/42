a0c802fb3f09cb3dd0eb42cba0849e2658d1c3ce
claudialbzz <claudialbombin@gmail.com>
2025-07-24 10:18:39 +0200

    arreglo en el factorial de 0

--- C05/ex01/ft_recursive_factorial.c
+++ C05/ex01/ft_recursive_factorial.c
@@ -6,17 +6,17 @@
 /*   By: claudialbombin <claudialbombin@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/20 19:59:36 by claudialbom       #+#    #+#             */
-/*   Updated: 2025/07/20 20:01:32 by claudialbom      ###   ########.fr       */
+/*   Updated: 2025/07/24 10:17:54 by claudialbom      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_recursive_factorial(int nb)
 {
-	if (nb <= 0)
+	if (nb < 0)
 		return (0);
 	else
 	{
-		if (nb == 1)
+		if (nb == 1 || nb == 0)
 			return (1);
 		else
 			return (nb * ft_recursive_factorial(nb - 1));
