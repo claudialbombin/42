0e2e09432c877e6337341fe4ac6143d5e9e521ca
claudialbzz <claudialbombin@gmail.com>
2026-06-26 16:23:00 +0200

    Fin libft

--- Cursus/Rank00/Libft/ft_lstmap.c
+++ Cursus/Rank00/Libft/ft_lstmap.c
@@ -3,51 +3,41 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstmap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: clopez-b <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: claudialbombin <claudialbombin@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/19 17:34:22 by clopez-b          #+#    #+#             */
-/*   Updated: 2026/06/19 17:34:31 by clopez-b         ###   ########.fr       */
+/*   Updated: 2026/06/26 16:21:56 by claudialbom      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdlib.h>
 
-static void	clear_list(t_list **lst, void (*del)(void *))
+static t_list	*clear_and_fail(t_list **lst, void *content,
+		void (*del)(void *))
 {
-	t_list	*tmp;
-
-	while (*lst)
-	{
-		tmp = (*lst)->next;
-		if (del)
-			del((*lst)->content);
-		free(*lst);
-		*lst = tmp;
-	}
-	*lst = NULL;
+	if (content)
+		del(content);
+	ft_lstclear(lst, del);
+	return (NULL);
 }
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_list;
 	t_list	*new_node;
-	void	*content;
+	void	*new_content;
 
-	if (!lst || !f)
-		return (NULL);
 	new_list = NULL;
+	if (!f || !del)
+		return (NULL);
 	while (lst)
 	{
-		content = f(lst->content);
-		new_node = ft_lstnew(content);
+		new_content = f(lst->content);
+		if (!new_content)
+			return (clear_and_fail(&new_list, NULL, del));
+		new_node = ft_lstnew(new_content);
 		if (!new_node)
-		{
-			if (del)
-				del(content);
-			clear_list(&new_list, del);
-			return (NULL);
-		}
+			return (clear_and_fail(&new_list, new_content, del));
 		ft_lstadd_back(&new_list, new_node);
 		lst = lst->next;
 	}
